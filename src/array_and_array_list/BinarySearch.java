c67d933d2f415c95518399e794ddb15b7accc92f
Nilesh Dhawale <n.dhawale@rnt.ai>
2024-06-25 16:45:46 +0530

    linear search and binary search

---
+++ src/array_and_array_list/BinarySearch.java
@@ -0,0 +1,39 @@
+package array_and_array_list;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+
+		// if data is sorted we can use it very effectively
+		// O(log n)
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		System.out.println("Index of 7 is :: " + getIndexOfElement(arr, 7));
+		System.out.println("Index of 10 is :: " + getIndexOfElement(arr, 10));
+		System.out.println("Index of 1 is :: " + getIndexOfElement(arr, 1));
+		System.out.println("Index of 100 is :: " + getIndexOfElement(arr, 100));
+
+	}
+
+	public static Integer getIndexOfElement(int[] arr, int element) {
+
+		// here we can use wile loop
+		int start = 0;
+		int end = arr.length - 1;
+		while (start <= end) {
+			// fine mid
+			int mid = (start + end) / 2;
+
+			if (arr[mid] == element)
+				return mid;
+			// if element is greater than mid element
+			if (element > arr[mid]) {
+				start = mid + 1;
+			} else {
+				// element is smaller than mid element
+				end = mid - 1;
+			}
+		}
+		return -1;
+	}
+}
---
+++ src/array_and_array_list/LinearSearch.java
@@ -0,0 +1,34 @@
+package array_and_array_list;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+
+		// linear search
+		// basically it will more usefull when we have unsorted data
+		// O(n)
+
+		Integer[] arr = { 1, 2, 4, 3, 9, 7, 8, 6 };
+
+		System.out.println("Best Condition :: " + getElementIndex(arr, 1));
+		System.out.println("Worst Condition :: " + getElementIndex(arr, 10));
+		System.out.println("Ideal Condition :: " + getElementIndex(arr, 3));
+
+	}
+
+	public static Integer getElementIndex(Integer[] arr, Integer element) {
+
+		if (arr.length == 0) {
+			return -1;
+		}
+
+		for (int intex = 0; intex < arr.length; intex++) {
+			if (arr[intex] == element) {
+				return intex;
+			}
+		}
+
+		return -1;
+
+	}
+}
