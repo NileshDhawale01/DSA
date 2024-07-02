61c2f7e6692eb2dc7d27e6f93a3eb37c62af16cd
nilesh dhawale <nileshdhawale1111@gmail.com>
2024-07-02 21:32:58 +0530

    selection sort

--- src/array_and_array_list/sort/SelectionSort.java
+++ src/array_and_array_list/sort/SelectionSort.java
@@ -1,5 +1,7 @@
 package array_and_array_list.sort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
@@ -13,5 +15,24 @@ public class SelectionSort {
 		// and put it in it's exact position
 		// time complexity is O(n^2)
 
+		int[] arr = { 1, 5, 4, 6, 3, 2, 9, 8, 7 };
+
+		int maxIndex = -1;
+		for (int i = 0; i < arr.length - 1; i++) {
+
+			maxIndex = i;
+			for (int j = i + 1; j < arr.length; j++) {
+
+				if (arr[j] < arr[maxIndex]) {
+					maxIndex = j;
+				}
+			}
+
+			// swap
+			int temp = arr[i];
+			arr[i] = arr[maxIndex];
+			arr[maxIndex] = temp;
+		}
+		System.out.println("Sorted Array Is :: " + Arrays.toString(arr));
 	}
 }
